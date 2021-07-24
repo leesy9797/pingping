498f3639633c821424f4923bfad4ca02466ec003
soyeon <86111748+leesy9797@users.noreply.github.com>
2021-07-24 16:37:45 +0900

    Delete Action.java

--- src/action/Action.java
+++
@@ -1,9 +0,0 @@
-package action;
-
-import javax.servlet.http.*;
-import vo.*;
-
-public interface Action {
-	ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-	
-}
